c8dd2045d32d946b11f527518f30df07853e4b07
MrMelon54 <github@mrmelon54.com>
2024-11-30 23:59:28 +0000

    Rewrite using astro

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+// @ts-check
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+
+// https://astro.build/config
+export default defineConfig({
+	site: 'https://1f349.com',
+	integrations: [mdx(), sitemap()],
+});
